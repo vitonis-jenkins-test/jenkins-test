b68fff9fc3e653036d09593ab0a6737db52ca1b2
Tony <misc.hole@mail.com>
2018-10-22 21:45:37 -0400

    Convert tabs to spaces.

--- jenkins-test.c
+++ jenkins-test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello from the Jenkins tester!\n");
-	printf("Also goodbye!\n");
-	return 0;
+   printf("Hello from the Jenkins tester!\n");
+   printf("Also goodbye!\n");
+   return 0;
 }
